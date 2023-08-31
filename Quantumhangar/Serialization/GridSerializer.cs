ff3dc219450e20c540718d579ca72f0092fe47a0
SentorX <66237290+buttheadbob@users.noreply.github.com>
2023-08-31 21:10:08 +0200

    Update for MyObjectBuilderSerializerKeen

--- Quantumhangar/Serialization/GridSerializer.cs
+++ Quantumhangar/Serialization/GridSerializer.cs
@@ -11,6 +11,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using VRage.Game;
 using VRage.ObjectBuilders;
+using VRage.ObjectBuilders.Private;
 using VRage.Utils;
 
 namespace QuantumHangar.Serialization
@@ -114,19 +115,19 @@ namespace QuantumHangar.Serialization
         private static bool SaveGridToFile(string savePath, string gridName,
             IEnumerable<MyObjectBuilder_CubeGrid> gridBuilders)
         {
-            var definition = MyObjectBuilderSerializer.CreateNewObject<MyObjectBuilder_ShipBlueprintDefinition>();
+            var definition = MyObjectBuilderSerializerKeen.CreateNewObject<MyObjectBuilder_ShipBlueprintDefinition>();
 
             definition.Id = new MyDefinitionId(new MyObjectBuilderType(typeof(MyObjectBuilder_ShipBlueprintDefinition)),
                 gridName);
             definition.CubeGrids = gridBuilders.ToArray();
             //PrepareGridForSave(definition);
 
-            var builderDefinition = MyObjectBuilderSerializer.CreateNewObject<MyObjectBuilder_Definitions>();
+            var builderDefinition = MyObjectBuilderSerializerKeen.CreateNewObject<MyObjectBuilder_Definitions>();
             builderDefinition.ShipBlueprints = new[] { definition };
 
 
             Log.Warn("Saving grid @" + Path.Combine(savePath, gridName + ".sbc"));
-            return MyObjectBuilderSerializer.SerializeXML(Path.Combine(savePath, gridName + ".sbc"), false,
+            return MyObjectBuilderSerializerKeen.SerializeXML(Path.Combine(savePath, gridName + ".sbc"), false,
                 builderDefinition);
         }
 
@@ -141,7 +142,7 @@ namespace QuantumHangar.Serialization
 
             try
             {
-                if (MyObjectBuilderSerializer.DeserializeXML(path, out MyObjectBuilder_Definitions def))
+                if (MyObjectBuilderSerializerKeen.DeserializeXML(path, out MyObjectBuilder_Definitions def))
                 {
                     if (!TryGetGridsFromDefinition(def, out grids))
                         return false;
